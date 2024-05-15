9d7536385246b2e865f9dbe751d61b88e279e864
Darwish <38754760+LonelyFellas@users.noreply.github.com>
2024-05-15 13:37:55 +0800

    feat: 修复发布的一些问题

--- electron/preload.ts
+++ electron/preload.ts
@@ -6,7 +6,7 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
   on(...args: Parameters<typeof ipcRenderer.on>) {
     const [channel, listener] = args;
     // 设置白名单，限制可以访问的channel
-    const whitelist = ['error'] as UnionToTuple<keyof OnChannelMap>;
+    const whitelist: UnionToTuple<keyof OnChannelMap> = ['error'];
     if (whitelist.includes(channel as any)) {
       return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args));
     }
@@ -24,7 +24,7 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
   send(...args: Parameters<typeof ipcRenderer.send>) {
     const [channel, ...omit] = args;
     // 同上
-    const whitelist = ['scrcpy:start', 'app:operate', 'loading:done'] as UnionToTuple<keyof SendChannelMap>;
+    const whitelist = ['scrcpy:start', 'app:operate', 'loading:done'] as unknown as UnionToTuple<keyof SendChannelMap>;
     if (whitelist.includes(channel as any)) {
       return ipcRenderer.send(channel, ...omit);
     }
@@ -33,7 +33,7 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
   invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
     const [channel, ...omit] = args;
     // 同上
-    const whitelist = ['window:state', 'dialog:open'] as UnionToTuple<keyof InvokeChannelMap>;
+    const whitelist: UnionToTuple<keyof InvokeChannelMap> = ['window:state', 'dialog:open'];
     if (whitelist.includes(channel as any)) {
       return ipcRenderer.invoke(channel, ...omit);
     }
--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         sourcemap: false,
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            return id.toString().split('node_module')[1].split('/')[0].toString();
           }
         },
       },
