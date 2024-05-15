cca1f6df0779d606028cbcf29a211a3ed1b94f4f
Darwish <38754760+LonelyFellas@users.noreply.github.com>
2024-05-15 11:32:09 +0800

    feat: 环境管理添加英文界面

--- src/pages/discover/proxy/index.tsx
+++ src/pages/discover/proxy/index.tsx
@@ -1,33 +1,99 @@
 import { useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import { Button, Space, Popconfirm } from 'antd';
 import { useNavigate } from '@tanstack/react-router';
-import { Table, TriggerModal } from '@common';
+import { Table, TriggerModal, useI18nConfig } from '@common';
 import { type GetProxyListResult, postDeleteProxyService } from '@api';
 import { postsProxyQueryOptions } from '@/routes/data';
-import { columns as configColumns } from './config';
 import AddEditModal from './modules/add-edit';
 
 export default function Proxy() {
+  const [lang] = useI18nConfig('config.proxy');
   const navigate = useNavigate();
   const { data, refetch, isLoading } = useSuspenseQuery(postsProxyQueryOptions);
   const deleteMutation = useMutation({
     mutationFn: postDeleteProxyService,
   });
-  const columns = configColumns.concat({
-    title: '操作',
+  const defaultColumns: AntdColumns<GetProxyListResult> = [
+    {
+      title: '#',
+      dataIndex: 'order',
+      key: 'order',
+      width: 80,
+      render: (_: unknown, __: unknown, index: number) => index + 1,
+    },
+    {
+      title: lang.column_info_title,
+      dataIndex: 'proxyInfo',
+      key: 'proxyInfo',
+      width: 200,
+      render: (_: unknown, record: GetProxyListResult) => {
+        const proxyType = record.type === '1' ? 'socks5' : 'https';
+        return (
+          <span>
+            {proxyType}://{record.address}:{record.port}
+          </span>
+        );
+      },
+    },
+    {
+      title: lang.column_type_title,
+      dataIndex: 'type',
+      key: 'type',
+      width: 150,
+      render: (text: string) => (text === '1' ? 'socks5' : 'https'),
+    },
+    {
+      title: lang.column_address_title,
+      dataIndex: 'address',
+      key: 'address',
+      width: 150,
+    },
+    {
+      title: lang.column_order_title,
+      dataIndex: 'id',
+      key: 'id',
+      width: 150,
+    },
+    // {
+    //   title: 'ip查询渠道',
+    //   dataIndex: 'queryChannel',
+    //   key: 'queryChannel',
+    //   width: 200,
+    // },
+    {
+      title: lang.column_remark_title,
+      dataIndex: 'detail',
+      key: 'detail',
+      width: 150,
+    },
+    // {
+    //   title: '云手机环境数',
+    //   dataIndex: 'cloudPhoneEnvNum',
+    //   key: 'cloudPhoneEnvNum',
+    //   width: 150,
+    // },
+    // {
+    //   title: '浏览器环境数',
+    //   dataIndex: 'browserEnvNum',
+    //   key: 'browserEnvNum',
+    //   width: 150,
+    // },
+  ];
+  const columns = defaultColumns.concat({
+    title: lang.tb_operation,
     dataIndex: 'action',
     key: 'action',
     width: 150,
     fixed: 'right',
     render: (_: unknown, record: GetProxyListResult) => (
       <Space>
-        <TriggerModal renderModal={(renderProps) => <AddEditModal {...renderProps} title="编辑代理" handleUpdateList={handleUpdateList} id={record.id} />}>
+        <TriggerModal renderModal={(renderProps) => <AddEditModal {...renderProps} title={lang.modal_edit_title} handleUpdateList={handleUpdateList} id={record.id} />}>
           <Button type="primary" ghost>
-            编辑
+            {lang.tb_operation_edit}
           </Button>
         </TriggerModal>
-        <Popconfirm title="确认删除？" onConfirm={() => handleDelete(record.id)} okText="确认" cancelText="取消">
-          <Button danger>删除</Button>
+        <Popconfirm title={lang.tb_operation_delete_title} onConfirm={() => handleDelete(record.id)} okText={lang.tb_operation_delete_okText} cancelText={lang.tb_operation_delete_cancelText}>
+          <Button danger>{lang.tb_operation_delete}</Button>
         </Popconfirm>
       </Space>
     ),
@@ -52,11 +118,11 @@ export default function Proxy() {
   return (
     <div className="flex flex-col p-2 h-full 2xl:p-4">
       <div className="">
-        <TriggerModal renderModal={(renderProps) => <AddEditModal {...renderProps} title="添加代理" handleUpdateList={handleUpdateList} />}>
-          <Button type="primary">添加代理</Button>
+        <TriggerModal renderModal={(renderProps) => <AddEditModal {...renderProps} title={lang.modal_add_title} handleUpdateList={handleUpdateList} />}>
+          <Button type="primary">{lang.modal_add_title}</Button>
         </TriggerModal>
         <Button className="ml-2 2xl:ml-4" onClick={handleGoToAddBatches}>
-          批量导入
+          {lang.modal_batch_add_title}
         </Button>
       </div>
       <Table loading={isLoading} rowKey={(record) => record.id} className="mt-2 2xl:mt-4" virtual columns={columns} dataSource={data} />
