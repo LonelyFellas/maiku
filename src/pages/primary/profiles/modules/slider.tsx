07215f5edf835b479115e867059f84fb94f01cc2
Darwish <38754760+LonelyFellas@users.noreply.github.com>
2024-05-15 15:01:12 +0800

    fix: 修改升级后的依赖倒置项目报错

--- src/pages/primary/profiles/modules/slider.tsx
+++ src/pages/primary/profiles/modules/slider.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { Button, Collapse, Flex, Image } from 'antd';
 import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from '@tanstack/react-router';
