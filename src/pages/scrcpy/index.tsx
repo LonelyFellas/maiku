a4f6059b020ea754eec28ab92221257e4aa3f873
Darwish <289582304@qq.com>
2024-05-30 09:43:26 +0800

    feat: 添加字体

--- src/pages/scrcpy/index.tsx
+++ src/pages/scrcpy/index.tsx
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
-import { Spin } from 'antd';
-import { useMatch, useRouter, useParams, useSearch } from '@tanstack/react-router';
+import { Flex, Spin } from 'antd';
 
 export default function ScrcpyWindow() {
   return (
     <div className="flex bg-[#d7dae3] h-full w-full">
-      {/*<ContainerWithEmpty className="w-[391px] bg-amber-400" isFetching description="正在启动云机"></ContainerWithEmpty>*/}
       <div className="all_flex  w-[381px] bg-white">
         <div className="text-center">
           <Spin size="large" />
           <h1 className="mt-2 text-primary">正在启动云机 ...</h1>
         </div>
       </div>
-      <div></div>
+      <div className="h-full flex flex-col justify-between text-[14px] items-center flex-1 text-sc">
+        <Flex vertical className="w-full text-center">
+          <div className="hover:bg-white text-sc">旋转</div>
+          <div className="hover:bg-white">截图</div>
+          <div className="hover:bg-white">上传</div>
+          <div className="hover:bg-white">音量</div>
+          <div className="hover:bg-white">音量</div>
+          <div className="hover:bg-white">加速</div>
+          <div className="hover:bg-white">更多</div>
+        </Flex>
+        <div className="w-full text-center hover:bg-white">更多</div>
+      </div>
     </div>
   );
 }
