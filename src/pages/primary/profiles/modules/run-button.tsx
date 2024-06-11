f01eed977896a534df981857b3aa08debcd0d9a9
Darwish <289582304@qq.com>
2024-06-11 13:58:47 +0800

    feat: 增加窗口尺寸的ui逻辑

--- src/pages/primary/profiles/modules/run-button.tsx
+++ src/pages/primary/profiles/modules/run-button.tsx
@@ -1,26 +1,30 @@
 import { type ButtonProps, Dropdown, Radio } from 'antd';
+import type { WindowsSize } from '../type.ts';
 
 interface RunButtonProps extends ButtonProps {
   isRunning: boolean;
   onRestartClick: GenericsFn;
+  windowSize: WindowsSize[keyof WindowsSize];
+  handleWindowsChange: (size: WindowsSize[keyof WindowsSize]) => void;
 }
 
 const RunButton: React.FC<RunButtonProps> = (props) => {
-  const { isRunning, onRestartClick, children, ...restProps } = props;
-
+  const { isRunning, onRestartClick, children, handleWindowsChange, windowSize, ...restProps } = props;
+  console.log(props.windowSize);
   const runningItems = [
     {
       key: '1',
       label: '重启',
     },
   ];
+
   const notRunningItems = [
     {
       key: 'mini-size',
       label: (
         <div>
           <span>窗口尺寸: </span>
-          <Radio.Group size="small" value="default">
+          <Radio.Group size="small" value={windowSize} onChange={(e) => handleWindowsChange(e.target.value)}>
             <Radio.Button value="small">小号</Radio.Button>
             <Radio.Button value="default">默认</Radio.Button>
             <Radio.Button value="large">大号</Radio.Button>
